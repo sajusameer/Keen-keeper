8528bf51239277d4c2faded4341735b14b417e57
sajusameer <sajudsameer32@gmail.com>
2026-04-17 03:26:22 +0600

    footer corection

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,50 +13,49 @@ export default function Footer() {
 
         {/* Subtitle */}
         <p className="text-gray-300 text-sm mb-6">
-          Track and maintain your meaningful relationships
+          Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
         </p>
 
-        {/* Social Icons */}
-        {/* Social Icons */}
-<div className="flex justify-center gap-4 mb-6">
-  <a
-    href="#"
-    className="w-9 h-9 flex items-center justify-center bg-white rounded-full text-black hover:scale-105 transition"
-  >
-    <FaFacebook size={16} />
-  </a>
+        
+      <div className="flex justify-center gap-4 mb-6">
+        <a
+          href="#"
+          className="w-9 h-9 flex items-center justify-center bg-white rounded-full text-black hover:scale-105 transition"
+        >
+          <FaFacebook size={16} />
+        </a>
 
-  <a
-    href="#"
-    className="w-9 h-9 flex items-center justify-center bg-white rounded-full text-black hover:scale-105 transition"
-  >
-    <FaTwitter size={16} />
-  </a>
+        <a
+          href="#"
+          className="w-9 h-9 flex items-center justify-center bg-white rounded-full text-black hover:scale-105 transition"
+        >
+          <FaTwitter size={16} />
+        </a>
 
-  <a
-    href="#"
-    className="w-9 h-9 flex items-center justify-center bg-white rounded-full text-black hover:scale-105 transition"
-  >
-    <FaInstagram size={16} />
-  </a>
-</div>
+        <a
+          href="#"
+          className="w-9 h-9 flex items-center justify-center bg-white rounded-full text-black hover:scale-105 transition"
+        >
+          <FaInstagram size={16} />
+        </a>
+      </div>
 
-        {/* Divider */}
+   
         <div className="border-t border-gray-500 w-70% mx-auto mb-4"></div>
 
-        {/* Bottom Row: Copyright + Menu */}
+        
         <div className="flex flex-col md:flex-row justify-around items-center text-xs text-gray-400 gap-3">
 
-          {/* Copyright */}
+         
           <p>
             © {new Date().getFullYear()} KeenKeeper. All rights reserved.
           </p>
 
           {/* Menu Links */}
           <div className="flex gap-6">
-            <Link to="/" className="hover:text-gray-300">Home</Link>
-            <Link to="/timeline" className="hover:text-gray-300">Timeline</Link>
-            <Link to="/stats" className="hover:text-gray-300">Stats</Link>
+            <Link to="/" className="hover:text-gray-300">Privacy Policy </Link>
+            <Link to="/" className="hover:text-gray-300">Terms of Service </Link>
+            <Link to="/" className="hover:text-gray-300">Cookies</Link>
           </div>
 
         </div>
